8847155451eb42ce391d673be01dc105792f658a
KNOWN-UNIVERSE\kenvorse <kenvorse@uat.edu>
2018-05-16 01:12:03 -0700

    Add project files.

---
+++ TestGround/Func.cpp
@@ -0,0 +1,5 @@
+#include"Func.h"  // includes just this project(Relative )
+int subNums(int a, int b) //cpp: Implementation : here is how they work and implement 
+{
+	return a - b;
+}
---
+++ TestGround/Func.h
@@ -0,0 +1,7 @@
+
+#ifndef FUNC_H //Header Guard (Protects Against Multiple Inclusion)
+#define FUNC_H
+#include<iostream>
+int subNums(int a, int b);//h : Definition we put every hting we need in the h.file
+#endif
+	
